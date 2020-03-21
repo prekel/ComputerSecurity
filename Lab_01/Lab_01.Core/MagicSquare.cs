54640fe6516e48b451f6fe613dc8bd4a5340ead6
Vladislav Prekel <misterptits@yandex.ru>
2020-03-21 21:18:51 +0700

    MagicSquare starts

--- Lab_01/Lab_01.Core/MagicSquare.cs
+++ Lab_01/Lab_01.Core/MagicSquare.cs
@@ -1,7 +1,71 @@
-﻿namespace Lab_01.Core
+﻿using System.Linq;
+using System;
+
+namespace Lab_01.Core
 {
     public class MagicSquare
     {
-        private int[,] data;
+        public int Count => _data.Length;
+        private readonly int[,] _data;
+
+        public MagicSquare(int[,] data)
+        {
+            _data = data;
+        }
+
+        public int this[int i, int j]
+        {
+            get => _data[i, j];
+            set => _data[i, j] = value;
+        }
+
+        public bool IsMagic
+        {
+            get { return true; }
+        }
+
+        private int SumRow(int i)
+        {
+            var s = 0;
+            for (var j = 0; j < Count; j++)
+            {
+                s += this[i, j];
+            }
+
+            return s;
+        }
+
+        private int SumColumn(int j)
+        {
+            var s = 0;
+            for (var i = 0; i < Count; i++)
+            {
+                s += this[i, j];
+            }
+
+            return s;
+        }
+
+        private int SumMainDiagonal()
+        {
+            var s = 0;
+            for (var i = 0; i < Count; i++)
+            {
+                s += this[i, i];
+            }
+
+            return s;
+        }
+
+        private int SumAntiDiagonal()
+        {
+            var s = 0;
+            for (var i = 0; i < Count; i++)
+            {
+                s += this[i, Count - i - 1];
+            }
+
+            return s;
+        }
     }
 }
