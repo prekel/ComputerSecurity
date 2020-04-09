392e0411500da4ac07167fb36115ab3324334d72
Vladislav Prekel <misterptits@yandex.ru>
2020-04-09 14:40:13 +0700

    almost done

--- Lab_01/Lab_01.App/MainWindow.xaml.cs
+++ Lab_01/Lab_01.App/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net.Mime;
 using System.Runtime.CompilerServices;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Lab_01.Core;
@@ -14,14 +16,31 @@ namespace Lab_01.App
     {
         private MagicSquareCipher Cipher { get; set; }
 
+        private bool IsRefreshKeyNeeded { get; set; } = true;
+
         private TextBox TextTextBox => this.FindControl<TextBox>("TextTextBox");
         private TextBox CiphertextTextBox => this.FindControl<TextBox>("CiphertextTextBox");
+        private TextBox FillerCharTextBox => this.FindControl<TextBox>("FillerCharTextBox");
         private TextBox KeyTextBox => this.FindControl<TextBox>("KeyTextBox");
         private ComboBox KeysComboBox => this.FindControl<ComboBox>("KeysComboBox");
+        private CheckBox RemoveSpacesCheckBox => this.FindControl<CheckBox>("RemoveSpacesCheckBox");
+        private CheckBox ToUpperCheckBox => this.FindControl<CheckBox>("ToUpperCheckBox");
+        private CheckBox RemoveNonLettersCheckBox => this.FindControl<CheckBox>("RemoveNonLettersCheckBox");
 
         public MainWindow()
         {
             InitializeComponent();
+            FillerCharTextBox
+                .GetObservable(TextBox.TextProperty)
+                .Subscribe(text => OnRefreshKeyNeeded());
+            KeyTextBox
+                .GetObservable(TextBox.TextProperty)
+                .Subscribe(text => OnRefreshKeyNeeded());
+        }
+
+        private void OnRefreshKeyNeeded()
+        {
+            IsRefreshKeyNeeded = true;
         }
 
         private void InitializeComponent()
@@ -42,17 +61,35 @@ namespace Lab_01.App
 
         private void RefreshKey()
         {
-            Cipher = new MagicSquareCipher(new MagicSquare(KeyTextBox.Text));
+            if (!IsRefreshKeyNeeded) return;
+            Cipher = new MagicSquareCipher(new MagicSquare(KeyTextBox.Text), FillerCharTextBox.Text.First());
+            IsRefreshKeyNeeded = false;
         }
 
-        private void OnRefreshKeyButtonClick(object sender, RoutedEventArgs e)
+        private string TransformString(string text)
         {
-            RefreshKey();
+            var ret = text;
+            if (RemoveSpacesCheckBox.IsChecked != null && RemoveSpacesCheckBox.IsChecked.Value)
+            {
+                ret = ret.Replace(" ", "");
+            }
+            if (RemoveNonLettersCheckBox.IsChecked != null && RemoveNonLettersCheckBox.IsChecked.Value)
+            {
+                ret = new String(ret.Where(Char.IsLetter).ToArray());
+            }
+            if (ToUpperCheckBox.IsChecked != null && ToUpperCheckBox.IsChecked.Value)
+            {
+                ret = ret.ToUpperInvariant();
+            }
+
+            return ret;
         }
 
         private void OnCryptButtonClick(object sender, RoutedEventArgs e)
         {
-            var text = TextTextBox.Text;
+            RefreshKey();
+
+            var text = TransformString(TextTextBox.Text);
 
             var ct = Cipher.Crypt(text);
 
@@ -61,6 +98,8 @@ namespace Lab_01.App
 
         private void OnEncryptButtonClick(object sender, RoutedEventArgs e)
         {
+            RefreshKey();
+
             var ct = CiphertextTextBox.Text;
 
             var text = Cipher.Encrypt(ct);
@@ -71,6 +110,7 @@ namespace Lab_01.App
         private void OnKeysComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             KeyTextBox.Text = KeysComboBox.SelectedItem + "\n";
+            IsRefreshKeyNeeded = true;
         }
     }
 }
