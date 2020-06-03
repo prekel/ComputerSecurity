acfbc808544c9810c7b6b6729fb34a70b5234458
Vladislav Prekel <misterptits@yandex.ru>
2020-06-03 17:20:53 +0700

    ui

--- Lab_03/Lab_03.App/MainWindow.xaml.cs
+++ Lab_03/Lab_03.App/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Linq;
 
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+
+using Lab_03.Core;
 
 using MessageBox.Avalonia;
 
@@ -15,18 +18,72 @@ namespace Lab_03.App
             InitializeComponent();
         }
 
+        private VigenereAnalysis VigenereAnalysis { get; set; } = new VigenereAnalysis("");
+
         private TextBox TextTextBox => this.FindControl<TextBox>("TextTextBox");
         private TextBox CiphertextTextBox => this.FindControl<TextBox>("CiphertextTextBox");
+        private ComboBox VariantsComboBox => this.FindControl<ComboBox>("VariantsComboBox");
+        private ComboBox MuComboBox => this.FindControl<ComboBox>("MuComboBox");
+        private TextBox MostOcurredLettersTextBox => this.FindControl<TextBox>("MostOcurredLettersTextBox");
+        private TextBox KeyTextBox => this.FindControl<TextBox>("KeyTextBox");
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            VariantsComboBox.Items = Enumerable.Range(1, Variants.GetVariantsCount());
         }
 
-        private void OnCryptButtonClick(object sender, RoutedEventArgs e)
+
+        private void OnVariantsComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
             {
+                CiphertextTextBox.Text = Variants.GetVariantByNumber((int) VariantsComboBox.SelectedItem);
+            }
+            catch (Exception ex)
+            {
+                ExceptionMessageBox(ex);
+            }
+        }
+
+        private void OnMuComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                VigenereAnalysis.SuggestMu((int) MuComboBox.SelectedItem);
+                KeyTextBox.Text = VigenereAnalysis.VigenereDecrypter.Key;
+                MostOcurredLettersTextBox.Text = new string('О', VigenereAnalysis.Mu);
+            }
+            catch (Exception ex)
+            {
+                ExceptionMessageBox(ex);
+            }
+        }
+
+        private void OnPossibleMusButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                VigenereAnalysis = new VigenereAnalysis(CiphertextTextBox.Text);
+                MuComboBox.Items = VigenereAnalysis.PossibleMus();
+            }
+            catch (Exception ex)
+            {
+                ExceptionMessageBox(ex);
+            }
+        }
+
+        private void OnSuggestMostOccuredButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                for (var i = 0; i < VigenereAnalysis.Mu; i++)
+                {
+                    VigenereAnalysis.SuggestMostOccuring(i, MostOcurredLettersTextBox.Text[i]);
+                }
+
+                KeyTextBox.Text = VigenereAnalysis.VigenereDecrypter.Key;
             }
             catch (Exception ex)
             {
@@ -38,6 +95,8 @@ namespace Lab_03.App
         {
             try
             {
+                VigenereAnalysis.VigenereDecrypter.Key = KeyTextBox.Text;
+                TextTextBox.Text = VigenereAnalysis.VigenereDecrypter.Text;
             }
             catch (Exception ex)
             {
